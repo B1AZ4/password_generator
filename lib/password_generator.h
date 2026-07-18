a6d3d584059b112cf2611bd39247705067852c00
Смоликов Роман <romansmolikov4@gmail.com>
2026-07-18 23:17:15 +0500

    password_generator.h

--- lib/password_generator.h
+++ lib/password_generator.h
@@ -4,3 +4,30 @@ password_generator.h - заголовочный файл модуля password_g
 Смоликов Роман
 МК-101
 */
+#ifndef PASSWORDGEN_H
+#define PASSWORDGEN_H
+#include <stdbool.h>
+
+#define MAX_DELIMITERS 256
+#define MAX_ALPHABET 512
+
+typedef struct {
+    int minl;
+    int maxl;
+    int length;
+    int count;
+    char alphabet[MAX_ALPHABET];
+    char charset_flags[16];
+
+    bool has_minl;
+    bool has_maxl;
+    bool has_n;
+    bool has_c;
+    bool has_a;
+    bool has_C;
+} Config;
+
+bool parse_arguments(int argc, char *argv[], Config* config);
+void generate_passwords(const Config* config);
+
+#endif
