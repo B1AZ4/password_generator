abd34a34877514b3f8754a19b5b009d792f16625
Смоликов Роман <romansmolikov4@gmail.com>
2026-07-19 19:30:05 +0500

    main.c

--- app/main.c
+++ app/main.c
@@ -4,3 +4,14 @@ main.c - основная программа
 Смоликов Роман
 МК-101
 */
+#include "password_generator.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    Config config;
+    if (!parse_arguments(argc, argv, &config)) {
+        return 1;
+    }
+    generate_passwords(&config);
+    return 0;
+}
