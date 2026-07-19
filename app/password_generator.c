c4526c10c829348c0e343a3a85ea4bfa00d42e84
Смоликов Роман <romansmolikov4@gmail.com>
2026-07-19 19:10:12 +0500

    password_gen: parser

--- app/password_generator.c
+++ app/password_generator.c
@@ -25,3 +25,48 @@ static bool is_numeric(const char* str) {
     }
     return true;
 }
+bool parse_arguments(int argc, char* argv[], Config* config) {
+    config->minl = -1; config->maxl = -1; config->length = -1; config->count = 1;
+    config->alphabet[0] = '\0'; config->charset_flags[0] = '\0';
+    config->has_minl = config->has_maxl = config->has_n = config->has_c = config->has_a = config->has_C = false;
+
+    char delims[MAX_DELIMITERS] = "=:";
+
+    for (int i = 1; i < argc; i++) {
+        char* arg = argv[i];
+        if (strncmp(arg, "-D", 2) == 0) {
+            char* val = NULL;
+            if (strlen(arg) > 2) {
+                if (is_delimiter(arg[2], "=:")) val = &arg[3];
+                else val = &arg[2];
+            }
+            else if (i + 1 < argc) {
+                val = argv[++i];
+            }
+            if (val == NULL || strlen(val) != 1) {
+                fprintf(stderr, "Ошибка:Некорректное значение для -D\n");
+                return false;
+            }
+            delims[0] = val[0]; delims[1] = '\0';
+        }
+        else if (strncmp(arg, "-d", 2) == 0) {
+            char* val = NULL;
+            if (strlen(arg) > 2) {
+                if (is_delimiter(arg[2], delims)) val = &arg[3];
+                else val = &arg[2];
+            }
+            else if (i + 1 < argc) {
+                val = argv[++i];
+            }
+            if (val == NULL || strlen(val) != 1) {
+                fprintf(stderr, "Ошибка:Некорректное значение для -d\n");
+                return false;
+            }
+            if (!is_delimiter(val[0], delims) && strlen(delims) < MAX_DELIMITERS - 1) {
+                int len = strlen(delims);
+                delims[len] = val[0]; delims[len + 1] = '\0';
+            }
+        }
+    }
+    return true;
+}
